3165fb41e57eac4c252a2ba28a54b632a72e18ac
Eshwar S B <esb753@email.com>
2024-05-29 15:58:00 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 // import dotenv from 'dotenv';
 // dotenv.config();
-
+import path from 'path';
 import connectDB from './config/db.js';
 import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
 import jobRoutes from './routers/jobRoutes.js'
